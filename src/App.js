092cfe435aec72fc4ed35355a42a67c750533711
aditya <adityaparsad01@gmail.com>
2020-10-08 13:19:58 +0530

    added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Reports from "./pages/Reports";
 import Products from "./pages/Products";
+import Team from "./pages/Team";
+import Contact from "./pages/Contact";
+import Support from "./pages/Support";
 
 export default function App() {
   return (
@@ -15,6 +18,9 @@ export default function App() {
           <Route path="/" component={Home} exact />
           <Route path="/products" component={Products} />
           <Route path="/reports" component={Reports} />
+          <Route path="/reports" component={Team} />
+          <Route path="/reports" component={Contact} />
+          <Route path="/reports" component={Support} />
         </Switch>
       </Router>
     </>
